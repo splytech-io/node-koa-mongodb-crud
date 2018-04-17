8a9a29a7d83f62a104ef9e375889a47eeeff079e
Andrius Skerla <andrius.skerla@gmail.com>
2018-04-17 09:49:02 +0100

    patch/put req fix

--- src/resource/put-resource.ts
+++ src/resource/put-resource.ts
@@ -51,7 +51,7 @@ export namespace PutResource {
         },
       }).catch(ResourceHelpers.handleDuplicateRecordError);
 
-      if (!result.modifiedCount) {
+      if (!result.matchedCount) {
         throw new ResourceError.NOT_FOUND();
       }
 
